0b4c5f37802e86d8c021fe37ae4cfa31e02434ee
Lewis Hazell <lewis.hazell@paramet.co.uk>
2022-07-13 23:03:47 +0100

    Implement file conversions.

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
-import { TEMPORARY_FILE_NAME, TEMPORARY_FOLDER_NAME } from "./defaults";
-import commandLineArgs from "command-line-args";
 import https from "https";
 import fs from "fs";
 import { ClientRequest } from "http";
+import commandLineArgs from "command-line-args";
 import jszip from "jszip";
+import resx from "resx";
+import { flatten } from "flat";
+import { TEMPORARY_FILE_NAME, TEMPORARY_FOLDER_NAME } from "./defaults";
 
 const options: commandLineArgs.CommandLineOptions = commandLineArgs([
     { name: "verbose", alias: "v", type: Boolean },
@@ -29,6 +31,23 @@ fileWriter.on("finish", async () => {
                     await item.async("arraybuffer")));
         }
     }
+
+    for (let map of options["map"]) {
+        const [infile, outfile]: [string, string] = map.split(":");
+        const indata = fs.readFileSync(`${TEMPORARY_FOLDER_NAME}/${infile}`);
+        const translations: any = flatten(JSON.parse(indata.toString()));
+
+        if(outfile.endsWith(".resx")) {
+            resx.js2resx(translations, (err: Error, res: string) => {
+                fs.writeFileSync(outfile, res);
+            });
+        } else if (outfile.endsWith(".json")) {
+            fs.writeFileSync(outfile, JSON.stringify(translations, null, 2));
+        }
+    }
+
+    fs.rmSync(TEMPORARY_FILE_NAME);
+    fs.rmSync(TEMPORARY_FOLDER_NAME, { recursive: true });
 });
 
 const request: ClientRequest = https.get(options["url"], function(response) {
