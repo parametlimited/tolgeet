6c33b9a5013a3a2be2160b3ac1f52b3b59bc55b9
Lewis Hazell <lewis.hazell@paramet.co.uk>
2022-07-15 14:51:32 +0100

    Add initial clean-up for failed tasks.

--- index.ts
+++ index.ts
@@ -9,11 +9,18 @@ import resx from "resx";
 import { flatten } from "flat";
 import { TEMPORARY_FILE_NAME, TEMPORARY_FOLDER_NAME } from "./defaults";
 
+function clean() {
+    fs.rmSync(TEMPORARY_FILE_NAME);
+    fs.rmSync(TEMPORARY_FOLDER_NAME, { recursive: true });
+}
+
 const options: commandLineArgs.CommandLineOptions = commandLineArgs([
     { name: "verbose", alias: "v", type: Boolean },
     { name: "map", alias: "m", type: String, multiple: true },
     { name: "url", type: String, defaultOption: true }
 ]);
+
+clean();
 
 const fileWriter: fs.WriteStream = fs.createWriteStream(TEMPORARY_FILE_NAME);
 fileWriter.on("finish", async () => {
@@ -48,8 +55,7 @@ fileWriter.on("finish", async () => {
         }
     }
 
-    fs.rmSync(TEMPORARY_FILE_NAME);
-    fs.rmSync(TEMPORARY_FOLDER_NAME, { recursive: true });
+    clean();
 });
 
 const request: ClientRequest = https.get(options["url"], function(response) {
